b61eb006066613a187999c62c914ccb9124e4354
lambistic14 <t0402188@my.ntu.ac.uk>
2026-05-21 14:58:35 +0100

    NicheRadar v1

---
+++ app/layout.js
@@ -0,0 +1,4 @@
+export const metadata = { title: "NicheRadar", description: "Branded Dropshipping Intelligence" };
+export default function RootLayout({ children }) {
+  return (<html lang="en"><body style={{ margin: 0, background: "#0B1120" }}>{children}</body></html>);
+}
